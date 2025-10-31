f9b164762dae7ac1bb022e3b76078b9fa9126ef9
kaua fidalgo <kauafidalgo01@gmail.com>
2025-10-31 09:17:22 -0300

    Exercicio BackEnd

---
+++ POO/PrimeiraClasse/Carro.cs
@@ -0,0 +1,25 @@
+namespace PrimeiraClasse
+{
+    public class Carro
+    {
+        
+        public string modelo;
+
+        public string marca;
+
+        public string cor;
+
+        public int potencia;
+
+        public void Ligar()
+        {
+            Console.WriteLine($"ziziziiz")
+
+        }
+
+        public void acelerar()
+        {
+            Console.WriteLine($"Vrum, leptop vrum")
+        }
+    }
+}
---
+++ POO/PrimeiraClasse/Program.cs
@@ -0,0 +1,22 @@
+﻿using PrimeiraClasse;
+
+Console.WriteLine($"===== PROGRAMA PRIMEIRA CLASSE =====");
+
+//Carro mclaren = new Carro();
+//mclaren.marca = "McLaren";
+//mclaren.cor = "Laranja";
+//mclaren.modelo = "750s"
+//mclaren.potencia = 800;
+
+
+//Console.WriteLine($"Ligando a {mclaren.marca} {mclaren.cor}")
+//mclaren.Ligar();
+//Console.WriteLine($"Ligando a {mclaren.marca} de {mclaren.potencia} cv")
+//mclaren.Acelerar();
+
+
+Pessoa fidalgo = new Pessoa();
+fidalgo.Nome = "Fidalgo";
+fidalgo.Idade = 16;
+fidalgo.Altura = 1.75;
+Console.WriteLine($"O nome do cidadão é {fidalgo.Nome} e ele tem {fidalgo.Idade} anos.");
